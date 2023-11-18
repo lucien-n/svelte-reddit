e2555e6f37c1be497898b4fbcb1c8d4f4ca12185
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-11-18 15:42:34 +0100

    feat: vector helper functions

--- src/lib/svos/helper.ts
+++ src/lib/svos/helper.ts
@@ -1,7 +1,13 @@
 import type { WindowStore } from './stores/window';
+import type { Vector } from './types';
 
 export const filterOutWindow = (windowId: string, windowStore: WindowStore) => {
 	let desktopWinId;
 	windowStore.subscribe(({ id }) => (desktopWinId = id));
 	return windowId !== desktopWinId;
+};
+
+export const vector = {
+	substract: (a: Vector, b: Vector) => [a[0] - b[0], a[1] - b[1]],
+	add: (a: Vector, b: Vector) => [a[0] + b[0], a[1] + b[1]]
 };
