c7dc5f41fa198860e48f471dd99dc90b7ae5d501
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-11-18 17:33:39 +0100

    ref: windows like taskbar icon

--- src/lib/svos/components/taskbar-slot.svelte
+++ src/lib/svos/components/taskbar-slot.svelte
@@ -9,14 +9,14 @@
 	on:click={() => win.focus()}
 	id="{$win.id}-taskbar-slot"
 	class={cn(
-		'relative h-full aspect-square hover:bg-foreground/10 group',
+		'relative h-full aspect-square hover:bg-foreground/10 group ease-in-out duration-200',
 		$win.isFocused && 'bg-foreground/30'
 	)}
 >
 	<div class="absolute bottom-0 flex w-full">
 		<div
 			class={cn(
-				'mx-auto group-hover:w-full transition-all ease-in-out duration-200 bg-muted-foreground h-1',
+				'mx-auto group-hover:w-full  ease-in-out duration-200 bg-muted-foreground h-1',
 				$win.isFocused ? 'w-full' : 'w-4/5'
 			)}
 		></div>
