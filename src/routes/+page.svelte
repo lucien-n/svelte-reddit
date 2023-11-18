36d973b7586c7aae10a4a18214302256ef0a4416
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-11-18 13:51:09 +0100

    feat: theme switcher

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+	import { Switch } from '$lib/components/ui/switch';
 	import Desktop from '$lib/svos/components/desktop.svelte';
+
+	const toggleTheme = () => {
+		const currentClass = document.children[0].className;
+		document.children[0].className = currentClass == 'dark' ? 'light' : 'dark';
+	};
 </script>
 
+<div class="absolute right-1 top-1">
+	<Switch id="theme" on:click={toggleTheme}></Switch>
+</div>
 <Desktop />
