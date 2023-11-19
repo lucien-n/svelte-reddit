03b4c642432be4e165aff7c8c0f649d8be9e43c7
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-11-19 12:09:23 +0100

    feat: auto hide taskbar

--- src/lib/svos/components/taskbar.svelte
+++ src/lib/svos/components/taskbar.svelte
@@ -1,10 +1,37 @@
 <script lang="ts">
+	import { flyAndScale } from '$lib/utils';
 	import { desktop } from '../stores/deskop';
 	import TaskbarSlot from './taskbar-slot.svelte';
+
+	export let autoHide: boolean = false;
+
+	let isHovering = false;
+
+	const handleMouseEnter = () => {
+		if (!autoHide) return;
+		isHovering = true;
+	};
+
+	const handleMouseLeave = () => {
+		if (!autoHide) return;
+		isHovering = false;
+	};
 </script>
 
-<section id="taskbar" class="flex mt-auto border-t z-20 h-16 w-full bg-background">
-	{#each $desktop.windows as win}
-		<TaskbarSlot {win} />
-	{/each}
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<section
+	id="taskbar"
+	class="flex mt-auto h-16 w-full m-3 z-20"
+	on:mouseenter={handleMouseEnter}
+	on:mouseleave={handleMouseLeave}
+>
+	{#if !autoHide || isHovering}
+		<div transition:flyAndScale={{ duration: 200 }} class="mx-auto">
+			<div class="border rounded h-16 bg-background">
+				{#each $desktop.windows as win (win.getField('id'))}
+					<TaskbarSlot {win} />
+				{/each}
+			</div>
+		</div>
+	{/if}
 </section>
